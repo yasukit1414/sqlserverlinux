cdc9ad8f43f3476b1770f8197efefe109af22560
Yasu <yasukit1414@hotmail.co.jp>
2017-08-15 14:43:23 +0900

    ver. 0.1

--- query/datetimegorup2.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-        CONVERT(NVARCHAR, ts, 111) AS 'access_date_char_yyyymmdd'
-      , DATEPART ( hour , ts ) as access_date_hour
-      ,count(*) as 'icount'
-  FROM expresslog
-group by
-        CONVERT(NVARCHAR, ts, 111) 
-      , DATEPART ( hour , ts ) 
-order by access_date_char_yyyymmdd,access_date_hour
--- query/datetimegroup.sql
+++
@@ -1,5 +0,0 @@
-SELECT datepart(minute,ts) As dategroup,Count(*)
- FROM expresslog
-WHERE ts BETWEEN '2016-6-26 00:34:51.00' And '2016-6-26 02:34:51.00'
-GROUP BY datepart(minute,ts) 
-ORDER BY dategroup
